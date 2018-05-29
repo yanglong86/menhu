070fc5d20c789e49c6d0271a941b76714b8c972f
yanglong86 <24125381527@qq,com>
2018-05-29 23:28:44 +0800

    zuuz

--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,16 @@ import Router from "vue-router";
 const _import = require("./_import_" + process.env.NODE_ENV);
 Vue.use(Router);
 export const constantRouterMap = [
+	{ path: '/', redirect: '/index' }, // 默认就跳转此页面
 	{
-		path: "/",
-		component: _import("index/index"),
-		name: "k8s",
-		children: [
-			{path: "/mainframe/:id", name: "流程配置", component: _import("index/environment/mainframe")},
-			{path: "/env", name: "环境配置", component: _import("index/environment/env/index")},
-			{path: "/host/:id", name: "主机列表页面", component: _import("index/environment/mainframe")},
-			{path: "/host/:id/:hostId", name: "主机详情", component: _import("index/environment/host")},
-			{
-				path: "/area/:id", name: "区域", component: _import("index/area/index"),
-				children: [
-					{path: "/area/:id/workloads", name: "载荷", component: _import("index/area/workloads/index")},
-					{path: "/area/:id/deployments", name: "部署", component: _import("index/area/deployments/index")},
-					{path: "/area/:id/deployments/:name", name: "部署详情", component: _import("index/area/deployments/info")},
-					{path: "/area/:id/pods", name: "容器组", component: _import("index/area/prod/index")},
-					{path: "/area/:id/pods/:name", name: "容器组详情", component: _import("index/area/prod/info")},
-					{path: "/area/:id/replicaset", name: "文本复制器", component: _import("index/area/replicaset/index")},
-					{path: "/area/:id/replicaset/:name", name: "文本复制器详情", component: _import("index/area/replicaset/info")},
-					{path: "/area/:id/services", name: "服务", component: _import("index/area/services/index")},
-					{path: "/area/:id/services/:name", name: "服务详情", component: _import("index/area/services/info")},
-				]
-			},
-			{
-				path:"/addhost/:id",
-				name:"添加主机",
-				component:_import("index/environment/add_host")
-			},
-			{
-				path:"/containerApp/:id",
-				name:"部署容器应用",
-				component:_import("index/environment/containerApp")
-			}
-
-		]
+		path: "/index",
+		component: _import("index/home/index"),
+		name: "首页"
+	},
+	{
+		path: "/aboutme",
+		component: _import("index/aboutme/index"),
+		name: "关于我们"
 	}
 ];
 
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,25 +1,16 @@
 <template>
     <div>
-        <sider-bar></sider-bar>
-        <el-row>
-            <el-col :xs="24" :sm="6" :md="6" :lg="4" :xl="4" style="padding: 15px;">
-                <nav-sidebar></nav-sidebar>
-            </el-col>
-            <el-col :xs="24" :sm="18" :md="18" :lg="20" :xl="20" style="padding: 15px">
-                <router-view></router-view>
-            </el-col>
-        </el-row>
-
+        222
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-	import siderBar from "./common/sider_bar"
-	import navSidebar from "./common/nav_sidebar"
+
 
 	export default {
 		name: "index",
-		components: {siderBar,navSidebar},
+		components: {},
 		data() {
 			return {}
 		},
